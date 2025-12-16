c578a427de4a76da773dbd87a9fe24ecd746b06c
khalathecoder <128059276+khalathecoder@users.noreply.github.com>
2025-12-15 23:21:50 -0500

    Users/khala/khalathecoder (#3)
    * comment out AddBlazoredTextEditor
    
    * fix journal page UI
    
    * add screenshot upload + preview to journal entries

---
+++ TradingROICalculator/Models/JournalEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace TradingROICalculator.Models
+{
+    public class JournalEntry
+    {
+        public DateTime CreatedUtc { get; set; }
+        public string Notes { get; set; } = string.Empty;
+        public string? Tags { get; set; }
+        public string? ScreenshotDataUrl { get; set; }
+    }
+}
